59d87a19ddd2cf7eb22f38a8ce2c3172d1e9e6a6
Denis Moroz <moroz.denis@gmail.com>
2017-08-08 07:06:31 +0300

    Simplify MPC params

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,21 +50,19 @@ class FG_eval {
     fg[0] = 0;
 
     for (int i = 0; i < N; i++) {
-      fg[0] += 3000*CppAD::pow(vars[cte_start + i] - ref_cte, 2);
-      fg[0] += 3000*CppAD::pow(vars[epsi_start + i] - ref_espi, 2);
-      fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
+      fg[0] += 165*CppAD::pow(vars[cte_start + i] - ref_cte, 2);
+      fg[0] += 1553*CppAD::pow(vars[epsi_start + i] - ref_espi, 2);
+      fg[0] += 0.0049*CppAD::pow(vars[v_start + i] - ref_v, 2);
     }
 
     for (int i = 0; i < N - 1; i++) {
-      fg[0] += 5*CppAD::pow(vars[delta_start + i], 2);
-      fg[0] += 5*CppAD::pow(vars[a_start + i], 2);
-
-      fg[0] += 700*CppAD::pow(vars[delta_start + i] * vars[v_start+i], 2);
+      fg[0] += 0.051*CppAD::pow(vars[delta_start + i], 2);
+      fg[0] += 0.012*CppAD::pow(vars[a_start + i], 2);
     }
 
     for (int i = 0; i < N - 2; i++) {
-      fg[0] += 200*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
-      fg[0] += 10*CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
+      fg[0] += 0.49*CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
+      fg[0] += CppAD::pow(vars[a_start + i + 1] - vars[a_start + i], 2);
     }
 
     // Initial constraints
